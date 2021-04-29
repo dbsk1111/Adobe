8aef938c69b0748bfd7da177eb2d99d0bd80f894
dbsk1111 <dbsk1111@gmail.com>
2021-04-30 08:29:24 +0900

    210430
    스크롤 효과 수정
    웹 표준 검사 후 html 조정 중

--- js/scroll.js
+++ js/scroll.js
@@ -2,36 +2,25 @@ $(function(){
   // 스크롤에 위치에 따라 on 클래스 넣기
   $(window).scroll(function(){
     let wHeight = $(this).height();
-    let thisScrollTop = $(this).scrollTop() + (wHeight / 1.5);
+    let thisScrollTop = $(this).scrollTop() + (wHeight / 1.8);
 
     $('section .box').each(function(){
       let thisOffset = $(this).offset().top;
-      if ( thisOffset <= thisScrollTop && thisScrollTop < thisOffset + wHeight ){
+      if ( thisOffset <= thisScrollTop ){
         $(this).addClass('on');
-      }else{
-        // $(this).removeClass('on');
       }
     })
   })
 
   $('a').click(function(){
-    let winHeight = $(window).height() + 60;
+    let winHeight = $(window).height();
     let getGoto = $(this).attr('href');
     let goTo = getGoto.substr(1);
-    console.log(goTo);
 
     $('section').each(function(){
       let secId = $(this).attr('id');
-      let secTop = $(this).offset().top;
-      console.log(secId);
-      // let wTop = window.scrollTop();
-      // console.log( "윈도우 스크롤 탑: "+wTop );
-      // let thisOffset = $(this).offset().top;
-      // console.log(thisOffset);
-      // console.log($(this).attr('id'));
-
+      let secTop = $(this).offset().top - 60;
       if( goTo == secId){
-        console.log('일치');
         window.scroll({ top: secTop, behavior: 'smooth' });
       }
     })
